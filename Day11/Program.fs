0b42cd4b2062049d8067b7738e92f282abb853cf
mark.bracke@tactics.be <mark.bracke@tactics.be>
2021-12-17 13:07:18 +0100

    Changed list of lists to an Array2D

--- Day11/Program.fs
+++ Day11/Program.fs
@@ -1,26 +1,22 @@
 ﻿/// https://adventofcode.com/2021/day/11
 module Day11
 open System
-open System.Collections.Generic
 
-let private readInput () = new List<List<int>> [|
-    for line in System.IO.File.ReadAllLines(System.IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "Day11", "Input.txt")) do
-        new List<int> [|
-            for character in line do Int32.Parse (character.ToString())
-        |]
-|]
+let private readInput () =
+    let lines = System.IO.File.ReadAllLines(System.IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "Day11", "Input.txt"))
+    Array2D.init (lines.Length) (lines[0].Length) (fun i j -> Int32.Parse (string (lines[i][j])))
 
 let part1 () =
     let input = readInput ()
 
     let calculateNbFlashes (nbIterations: int) =
         let rec increaseNumber (i: int, j: int) =
-            if (i < 0 || i = input.Count || j < 0 || j = input[i].Count) then
+            if (i < 0 || i = input.GetLength(0) || j < 0 || j = input.GetLength(1)) then
                 ()
             else
-                match input[i][j] with
+                match input[i, j] with
                 | 9 ->
-                    input[i][j] <- 10
+                    input[i, j] <- 10
                     increaseNumber (i-1, j)
                     increaseNumber (i, j-1)
                     increaseNumber (i+1, j)
@@ -32,15 +28,15 @@ let part1 () =
                 | 10 ->
                     ()
                 | other ->
-                    input[i][j] <- other + 1
+                    input[i, j] <- other + 1
                     ()
 
         let countFlashesAndReset () =
             [
-                for i in 0..input.Count - 1 do
-                    for j in 0..input[i].Count - 1 do
-                        if input[i][j] = 10 then
-                            input[i][j] <- 0
+                for i in 0..input.GetUpperBound(0) do
+                    for j in 0..input.GetUpperBound(1) do
+                        if input[i, j] = 10 then
+                            input.SetValue (0, i, j)
                             1
                         else
                             0
@@ -50,8 +46,8 @@ let part1 () =
         let rec calculateNbFlashes (nbFlashes: int, currentIteration: int) =
             if currentIteration = nbIterations then nbFlashes
             else
-                for i in 0..input.Count-1 do
-                    for j in 0..input.Count-1 do
+                for i in 0..input.GetUpperBound(0) do
+                    for j in 0..input.GetUpperBound(1) do
                         increaseNumber (i, j)
                 calculateNbFlashes (countFlashesAndReset (), currentIteration + 1)
 
@@ -62,18 +58,18 @@ let part1 () =
 
 let part2 () =
     let input = readInput ()
-    let nbNumbersInInput = input.Count * input[0].Count
+    let nbNumbersInInput = input.Length
 
     let calculateSynchronizationPoint () =
         let rec increaseNumber (i: int, j: int) =
-            if (i < 0 || i = input.Count || j < 0 || j = input[i].Count) then
+            if (i < 0 || i = input.GetLength(0) || j < 0 || j = input.GetLength(1)) then
                 ()
             else
-                match input[i][j] with
+                match input[i, j] with
                 | 10 ->
                     ()
                 | 9 ->
-                    input[i][j] <- 10
+                    input.SetValue(10, i, j)
                     increaseNumber (i-1, j)
                     increaseNumber (i, j-1)
                     increaseNumber (i+1, j)
@@ -83,27 +79,26 @@ let part2 () =
                     increaseNumber (i+1, j-1)
                     increaseNumber (i-1, j+1)
                 | other ->
-                    input[i][j] <- other + 1
+                    input.SetValue(other + 1, i, j)
                     ()
 
+        //Using mutable here to not have to go over the entire array twice...
         let countFlashesAndReset () =
-            [
-                for i in 0..input.Count - 1 do
-                    for j in 0..input[i].Count - 1 do
-                        if input[i][j] = 10 then
-                            input[i][j] <- 0
-                            1
-                        else
-                            0
-            ]
-            |> List.sum
+            let mutable counter = 0
+            input
+            |> Array2D.iteri (fun i j value ->
+                if value = 10 then
+                    counter <- counter + 1
+                    input.SetValue(0, i, j)
+                else
+                    ()
+            )
+            counter
 
         let rec calculateSynchronizationPoint (nbFlashes: int, currentIteration: int) =
             if nbFlashes = nbNumbersInInput then currentIteration
             else
-                for i in 0..input.Count-1 do
-                    for j in 0..input.Count-1 do
-                        increaseNumber (i, j)
+                input |> Array2D.iteri (fun i j _ -> increaseNumber (i, j))
                 calculateSynchronizationPoint (countFlashesAndReset (), currentIteration + 1)
 
         calculateSynchronizationPoint (0, 0)
