689dcaa35c5108a423d5483222aedf3af86931c6
mark.bracke@tactics.be <mark.bracke@tactics.be>
2021-12-17 13:13:58 +0100

    Replaced list of lists with Array2D

--- Day9/Program.fs
+++ Day9/Program.fs
@@ -1,7 +1,6 @@
 ﻿/// https://adventofcode.com/2021/day/9
 module Day9
 open System
-open System.Collections.Generic
 
 let part1 () =
     let heightMap: int[][] = [|
@@ -39,31 +38,29 @@ type Point =
     member me.Impassible = me.Number = 9
 
 let part2 () =
-    let heightMap = new List<List<Point>> [|
-        for line in System.IO.File.ReadAllLines(System.IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "Day9", "Input.txt")) do
-            new List<Point> [|
-                for character in line do
-                    { Number = Int64.Parse (character.ToString()); Visited = false }
-            |]
-    |]
+    let heightMap =
+        let input = System.IO.File.ReadAllLines(System.IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "Day9", "Input.txt"))
+        Array2D.init (input.Length) (input[0].Length) (fun i j ->
+            { Number = Int64.Parse (string (input[i][j])); Visited = false }
+        )
 
     let countBasin (i: int, j: int): Int64 =
         let rec visitNeighbor (i: int, j: int) =
-            if i < 0 || i >= heightMap.Count || j < 0 || j >= heightMap[i].Count then
+            if i < 0 || i >= heightMap.GetLength(0) || j < 0 || j >= heightMap.GetLength(1) then
                 0
             else
-                let currentPoint = heightMap[i][j]
+                let currentPoint = heightMap[i, j]
                 if currentPoint.Impassible || currentPoint.Visited then
                     0
                 else
-                    heightMap[i][j] <- { currentPoint with Visited = true }
+                    heightMap[i, j] <- { currentPoint with Visited = true }
                     1 + visitNeighbor (i+1, j) + visitNeighbor (i, j+1) + visitNeighbor (i-1, j) + visitNeighbor (i, j-1)
         visitNeighbor (i, j)
 
     let basinSizes = seq {
-        for i in 0..heightMap.Count-1 do
-            for j in 0..heightMap[i].Count-1 do
-                let currentNumber = heightMap[i][j]
+        for i in 0..heightMap.GetUpperBound(0) do
+            for j in 0..heightMap.GetUpperBound(1) do
+                let currentNumber = heightMap[i, j]
                 if currentNumber.Impassible || currentNumber.Visited then
                     ()
                 else
