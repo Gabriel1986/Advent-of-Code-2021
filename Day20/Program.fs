83d157b91d3f21107d36bc5dff93347944ef06df
mark.bracke@tactics.be <mark.bracke@tactics.be>
2021-12-20 13:17:43 +0100

    Made code more FP'y

--- Day20/Program.fs
+++ Day20/Program.fs
@@ -14,45 +14,40 @@ let private readInput (): string * char[][] =
             |]
         |]
 
-let enhance (enhancer: string) (image: char[][]) (iteration: int) =
-    let originalMaxI = image.Length - 1
-    let originalMaxJ = image[0].Length - 1
+let enhance (times: int) (enhancer: string, image: char[][]) =
+    let enhance (image: char[][]) (iteration: int) =
+        let originalMaxI = image.Length - 1
+        let originalMaxJ = image[0].Length - 1
 
-    [|
-        for i in -1..originalMaxI+1 do [|
-            for j in -1..originalMaxJ+1 do
-                let square = String [|
-                    for squareI in -1..1 do
-                        for squareJ in -1..1 do
-                            let originalI, originalJ = i + squareI, j + squareJ
-                            if originalI < 0 || originalI > originalMaxI || originalJ < 0 || originalJ > originalMaxJ then
-                                if enhancer[0] = '#' && iteration % 2 = 1 then '0' else '1'
-                            elif image[originalI][originalJ] = '.' then
-                                '0'
-                            else
-                                '1'
-                |]
-
-                enhancer[Convert.ToInt32(square, 2)]
+        [|
+            for i in -1..originalMaxI+1 do [|
+                for j in -1..originalMaxJ+1 do
+                    let square = String [|
+                        for squareI in -1..1 do
+                            for squareJ in -1..1 do
+                                let originalI, originalJ = i + squareI, j + squareJ
+                                if originalI < 0 || originalI > originalMaxI || originalJ < 0 || originalJ > originalMaxJ then
+                                    if enhancer[0] = '#' && iteration % 2 = 1 then '0' else '1'
+                                elif image[originalI][originalJ] = '.' then
+                                    '0'
+                                else
+                                    '1'
+                    |]
+                    enhancer[Convert.ToInt32(square, 2)]
+            |]
         |]
-    |]
+
+    seq { 1..times }
+    |> Seq.fold enhance image
 
 let part1 () =
-    let enhancer, image = readInput()
-
-    let enhancedImage =
-        image
-        |> (fun expanded -> seq { 1..2 } |> Seq.fold (enhance (enhancer)) expanded)
-
-    let nbLitPixels = enhancedImage |> Array.sumBy (Array.sumBy (function '#' -> 1 | _ -> 0))
-    printfn "Day 20 - Part 1 - Nb lit pixels: %i" nbLitPixels
+    readInput ()
+    |> enhance 2
+    |> Array.sumBy (Array.sumBy (function '#' -> 1 | _ -> 0))
+    |> printfn "Day 20 - Part 1 - Nb lit pixels: %i"
 
 let part2 () =
-    let enhancer, image = readInput()
-
-    let enhancedImage =
-        image
-        |> (fun expanded -> seq { 1..50 } |> Seq.fold (enhance (enhancer)) expanded)
-
-    let nbLitPixels = enhancedImage |> Array.sumBy (Array.sumBy (function '#' -> 1 | _ -> 0))
-    printfn "Day 20 - Part 2 - Nb lit pixels: %i" nbLitPixels
+    readInput ()
+    |> enhance 50
+    |> Array.sumBy (Array.sumBy (function '#' -> 1 | _ -> 0))
+    |> printfn "Day 20 - Part 2 - Nb lit pixels: %i"
